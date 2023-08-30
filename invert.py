794712ab0b5176d2ded4a45bbce2f0b7253b9a61
Hakase <davidsommerfield.zg@gmail.com>
2023-08-30 17:42:27 -0500

    dfs invert tree

---
+++ invert.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        def invert(root):
+            if root:
+                root.left, root.right = root.right, root.left
+                invert(root.left)
+                invert(root.right)
+
+        invert(root)    
+        
+        return root
