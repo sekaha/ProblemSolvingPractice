d7adc6db3fb544a93962354429275009e1663d5d
Hakase <davidsommerfield.zg@gmail.com>
2023-08-31 17:28:17 -0500

    this is what happens when i try to force my attention xD. I finally payed attention.

--- diameteroftree.py
+++ diameteroftree.py
@@ -7,16 +7,17 @@
 class Solution(object):
     def diameterOfBinaryTree(self, root):
         def get_depth_and_diameter(node):
-            l_dep, l_dia, r_dep, r_dia = -1, -1, -1, -1
+            l_dep, r_dep, l_dia, r_dia = 0, 0, 0, 0
 
             if node.left:
                 l_dep, l_dia = get_depth_and_diameter(node.left)
-
+            
             if node.right:
                 r_dep, r_dia = get_depth_and_diameter(node.right)
-            
-            pot_dia = (l_dep+1) + (r_dep+1)
 
-            return max(r_dep,l_dep)+1, max(l_dia+1,r_dia+1,pot_dia)
+            dep = max(l_dep,r_dep)+1
+            dia = max(r_dep+l_dep, r_dia, l_dia)
             
-        return get_depth_and_diameter(root)[0]
+            return dep, dia
+
+        return get_depth_and_diameter(root)[1]
