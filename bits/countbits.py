90bb86110166bcd8a2bf3c3e6659428763f85343
Hakase <davidsommerfield.zg@gmail.com>
2023-09-06 14:33:04 -0500

    a good solution

--- bits/countbits.py
+++ bits/countbits.py
@@ -2,13 +2,13 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        offset = 1
         ans = [0]*(n+1)
 
-        for i in range(n+1):
-            tmp = i
-
-            while tmp:
-                ans[i]+=1
-                tmp &= (tmp-1)
+        for i in range(1,n+1):
+            if i == offset*2:
+                offset *= 2
+            
+            ans[i] = 1+ans[i-offset]
 
         return ans
