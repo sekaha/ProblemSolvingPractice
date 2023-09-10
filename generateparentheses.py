35dabc0043574dfde6274540b3075a8bc4e821b3
Hakase <davidsommerfield.zg@gmail.com>
2023-09-10 02:09:26 -0500

    improved simpler solution

--- generateparentheses.py
+++ generateparentheses.py
@@ -2,20 +2,15 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def gen(left,right="",out=""):
+        def gen(left,right,out=""):
+            if not left and not right:
+                yield out
             if left:
-                if right:
-                    for push in range(2):
-                        if push:
-                            yield from gen(left[1:],right+")",out+"(")
-                        else:
-                            yield from gen(left,right[1:],out+")")
-                else:
-                    yield from gen(left[1:],right+")",out+"(")
-            else:
-                yield out+right
-
-        return list(gen("("*n))
+                yield from gen(left-1,right,out+"(")
+            if right > left:
+                yield from gen(left,right-1,out+")")
+        
+        return list(gen(n,n))
 
 sol = Solution()
 print(sol.generateParenthesis(3))
