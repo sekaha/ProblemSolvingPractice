9af1622940c875cad7484f98aa323ac05eac8c4d
Hakase <davidsommerfield.zg@gmail.com>
2023-09-11 15:27:28 -0500

    improved binomial solution

--- uniquepaths.py
+++ uniquepaths.py
@@ -2,17 +2,21 @@ from functools import reduce
 
 
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        def choose(n, k):
-            fact = (
-                lambda x: reduce(lambda a, b: a * b, range(1, x + 1)) if (x > 0) else 1
-            )
-            return int(fact(n) / (fact(k) * fact(n - k)))
+    def uniquePaths(self, w: int, h: int) -> int:
+        # finding the natural, triangular, tetrahedral etc numbers depending on inputs
+        n, k = h + (w - 2), w - 1
+        fact = 1
 
-        return choose(n + (m - 2), m - 1)
+        # binomial coefficinet
+        for i in range(1, k + 1):
+            # n! / (n-k)!, because this part gets rid of the (n-k) numbers from n!
+            fact *= n - i + 1
 
+            # / k!
+            fact //= i
 
-sol = Solution()
+        return fact
+
 
 # for x in range(1, 5):
 #    for y in range(1, 10):
