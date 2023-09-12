455803949653c9f49178fb03ca6f665bcc14d39e
Hakase <davidsommerfield.zg@gmail.com>
2023-09-11 19:54:46 -0500

    not it

--- longestrepeatingchar.py
+++ longestrepeatingchar.py
@@ -1,19 +1,30 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         queue = []
-        max_size = 0
-        sizes = {}
+        max_size = 1
+        sizes = {c: 1 for c in set(s)}
+        budgets = {c: k for c in set(s)}
 
-        for i, c in enumerate(s):
-            if c in queue:
+        for i, c in enumerate(s[1:]):
+            if c == s[i - 1]:
+                print("added")
+                sizes[c] += 1
+            elif c in queue and budgets[c]:
+                print("added")
+                budgets[c] -= 1
                 sizes[c] += 1
             else:
-                sizes[c] == 0s
+                budgets[c] = k
+                sizes[c] = 1
+
+            max_size = max(sizes[c], max_size)
             queue.append(c)
 
             if i > k:
                 queue.pop(0)
 
+        return max_size + 1
+
 
 sol = Solution()
-sol.characterReplacement("ABAB", 2)
+print(sol.characterReplacement("AABABBAAAAAAAABA", 2))
