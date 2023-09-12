3a93d2c61e53c2bf2aa578fa653d511f690189b5
Hakase <davidsommerfield.zg@gmail.com>
2023-09-12 11:07:22 -0500

    another attempt

--- longestrepeatingchar.py
+++ longestrepeatingchar.py
@@ -1,24 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         counts = {c: 0 for c in set(s)}
-        counts[s[0]] = 1
-        max_length = 0
-        l = 0
+        most_frequent = None
+        dist = 0
+        l, r = 0, 0
 
-        # maybe use a max heap
-        for r in range(len(s)):
-            # check if the amount of max occuring chars in the string
-            # make up len-k elements of that string, otherwise, start moving left pointer
-            focus = max(list(counts.keys()), key=lambda x: counts[x])[0]
-            while counts[focus] <= r - l - k:  # traversed thus far -k
+        while l != len(s) and r != len(s):
+            most_frequent = max(counts.values())
+
+            while r - l - k < most_frequent:
+                counts[s[r]] += 1
+                r += 1
+                dist = max(r - l, dist)
+            else:
+                counts[s[l]] -= 1
                 l += 1
-                counts[s[1]] -= 1
-                focus = max(list(counts.keys()), key=lambda x: counts[x])[0]
-            max_length = max(r - l, max_length)
-            counts[s[r]] += 1
 
-        return max_length
+        return dist
 
 
 sol = Solution()
-print(sol.characterReplacement("ABAB", 2))
+print(sol.characterReplacement("AABABBA", 1))
