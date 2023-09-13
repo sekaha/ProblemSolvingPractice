2208382d28265cd3376a47a2c7624e98252bab3e
Hakase <davidsommerfield.zg@gmail.com>
2023-09-12 23:49:47 -0500

    finally solved

--- longestrepeatingchar.py
+++ longestrepeatingchar.py
@@ -1,23 +1,25 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         counts = {c: 0 for c in set(s)}
-        most_frequent = None
-        dist = 0
         l, r = 0, 0
+        max_element = 0
+        max_length = 0
 
-        while l != len(s) and r != len(s):
-            most_frequent = max(counts.values())
+        # max f doesn't need to be proper, so we can just keep max_f constant even if it's not true for our window
+        while r < len(s):
+            counts[s[r]] += 1
+            max_element = max(max_element, counts[s[r]])
+            r += 1
 
-            # valid window
-            if (r - l) - most_frequent <= k:
-                counts[s[r]] += 1
-                r += 1
-            else:  # invalid window, shift until valid
+            # invalid window, shift until valid
+            while (r - l) - max_element > k:
                 counts[s[l]] -= 1
                 l += 1
 
-        return dist
+            max_length = max(max_length, r - l)
+
+        return max_length
 
 
 sol = Solution()
-print(sol.characterReplacement("AAAA", 2))
+print(sol.characterReplacement("AABABBA", 1))
