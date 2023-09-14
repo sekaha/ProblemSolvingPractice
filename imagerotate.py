161b4c75a06946f5c26e645bf7d3a0fff9542d27
Hakase <davidsommerfield.zg@gmail.com>
2023-09-14 09:52:52 -0500

    an alternative approach

--- imagerotate.py
+++ imagerotate.py
@@ -6,20 +6,15 @@ class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         N = len(matrix)
 
-        # iterate to cut out an upper triangle then swap that triangle iteratively until rotate
         for y in range(N // 2):
             for x in range(y, N - y - 1):
-                rot_x, rot_y = x, y
-
-                for i in range(3):
-                    matrix[rot_y][rot_x], matrix[-rot_x - 1][rot_y] = (
-                        matrix[-rot_x - 1][rot_y],
-                        matrix[rot_y][rot_x],
-                    )
-                    rot_x, rot_y = rot_y, -rot_x - 1
+                first = matrix[y][x]
+                matrix[y][x] = matrix[-x - 1][y]
+                matrix[-x - 1][y] = matrix[-y - 1][-x - 1]
+                matrix[-y - 1][-x - 1] = matrix[x][-y - 1]
+                matrix[x][-y - 1] = first
 
 
 sol = Solution()
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 sol.rotate(matrix)
-print(matrix)
