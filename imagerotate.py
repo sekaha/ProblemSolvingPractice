f18e65d41e8db1e4f3ee4545ad0660226b796522
Hakase <davidsommerfield.zg@gmail.com>
2023-09-14 09:40:45 -0500

    answer complete B)

--- imagerotate.py
+++ imagerotate.py
@@ -4,33 +4,22 @@ import numpy as np
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        N = 10  # len(matrix)
-        test = [[0] * N for n in range(N)]
-        visited = set()
+        N = len(matrix)
 
-        for x in range(N):
-            for y in range(N):
-                if (x, y) not in visited:
-                    test[x][y] = 1
-
-                    cur_x, cur_y = x, y
-
-                    for i in range(4):
-                        visited.add((cur_x, cur_y))
-                        # application of 90 deg rotation matrix, plus some tomfoolery since we have to do zero indexing
-                        cur_x, cur_y = (N - cur_y - 1), cur_x
-
-        test2 = [[0] * N for n in range(N)]
-
+        # iterate to cut out an upper triangle then swap that triangle iteratively until rotate
         for y in range(N // 2):
             for x in range(y, N - y - 1):
-                test2[y][x] = 1
+                rot_x, rot_y = x, y
 
-        test, test2 = np.array(test), np.array(test2)
-
-        print(np.array_equal(test, test2))
+                for i in range(3):
+                    matrix[rot_y][rot_x], matrix[-rot_x - 1][rot_y] = (
+                        matrix[-rot_x - 1][rot_y],
+                        matrix[rot_y][rot_x],
+                    )
+                    rot_x, rot_y = rot_y, -rot_x - 1
 
 
 sol = Solution()
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 sol.rotate(matrix)
+print(matrix)
