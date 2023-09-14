772b23b8d3f64fe0b2f69f0c92cd234958c1712f
Hakase <davidsommerfield.zg@gmail.com>
2023-09-14 10:21:26 -0500

    i kinda code golfed this one, how silly

--- DP/uniquepaths.py
+++ DP/uniquepaths.py
@@ -1,19 +1,13 @@
 class Solution:
     def uniquePaths(self, w: int, h: int) -> int:
-        # finding the simplex numbers (natural, triangular, tetrahedral...)  depending on inputs
-        n, k = h + w - 2, w - 1
-        result = 1
+        w, h, result = w - 1, h - 1, 1
 
-        # n choose k
-        for i in range(k):
-            # n! / (n-k)!, because this part gets rid of the (n-k) numbers from n!
-            result *= n - i
-
-            # / k!
+        for i in range(w):
+            result *= (h + w) - i
             result //= i + 1
 
         return result
 
 
 sol = Solution()
-print(sol.uniquePaths(3, 7))
+print(sol.uniquePaths(10, 10))
