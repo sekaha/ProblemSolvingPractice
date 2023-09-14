08a24322df9f85d70d622337e3685ea5d4b4eaa8
Hakase <davidsommerfield.zg@gmail.com>
2023-09-14 07:28:55 -0500

    final version

--- numbersofislands.py
+++ numbersofislands.py
@@ -3,27 +3,19 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
         islands = 0
 
         def check_neighbors(x, y):
-            for x_off, y_off in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                if (0 <= y + y_off < len(grid)) and (0 <= x + x_off < len(grid[0])):
-                    if (x + x_off, y + y_off) not in visited:
-                        if grid[y + y_off][x + x_off] == "1":
-                            visited.add((x + x_off, y + y_off))
-                            check_neighbors(x + x_off, y + y_off)
+            if 0 <= y < len(grid) and 0 <= x < len(grid[0]) and grid[y][x] == "1":
+                grid[y][x] = "0"
+                for x_off, y_off in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    check_neighbors(x + x_off, y + y_off)
 
         for y, row in enumerate(grid):
             for x, land in enumerate(row):
-                if (x, y) not in visited:
-                    visited.add((x, y))
-
-                    if land == "1":
-                        check_neighbors(x, y)
-                        islands += 1
-
-        check_neighbors(0, 0)
+                if land == "1":
+                    check_neighbors(x, y)
+                    islands += 1
 
         return islands
 
