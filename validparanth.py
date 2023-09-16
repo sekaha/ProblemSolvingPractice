3cc0a483f4ae8c7789a98a6361e2c19fecef9c9a
Hakase <davidsommerfield.zg@gmail.com>
2023-09-15 19:40:39 -0500

    solution

--- validparanth.py
+++ validparanth.py
@@ -1,42 +1,35 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        memo = {}
+        min_lefts = 0
+        max_lefts = 0
 
-        def backtracking(i=0, weight=0):
-            if i == len(s):
-                return weight == 0
-
-            if (i, weight) in memo:
-                return memo[i, weight]
+        for c in s:
+            if c == "(":
+                max_lefts += 1
+                min_lefts += 1
+            elif c == ")":
+                max_lefts -= 1
+                min_lefts -= 1
             else:
-                if s[i] == "(":
-                    validity = backtracking(i + 1, weight + 1)
+                min_lefts -= 1
+                max_lefts += 1
 
-                if s[i] == ")":
-                    if weight > 0:
-                        validity = backtracking(i + 1, weight - 1)
-                    else:
-                        validity = False
+            if min_lefts < 0:
+                min_lefts = 0
 
-                if s[i] == "*":
-                    validity = backtracking(i + 1, weight + 1)
-
-                    if weight > 0:
-                        validity |= backtracking(i + 1, weight - 1)
-
-                    validity |= backtracking(i + 1, weight)
-
-                memo[(i, weight)] = validity
-
-                return validity
-
-        return backtracking()
+            if max_lefts < 0:
+                return False
+                if min_lefts > 0:
+                    min_lefts -= 1
+        return min_lefts <= 0 <= max_lefts
 
 
 sol = Solution()
 
 print(sol.checkValidString("(*()))*("))
 print(sol.checkValidString("*)*())"))
+print(sol.checkValidString("(())"))
+print(sol.checkValidString("))(("))
 print(sol.checkValidString("()"))
 print(sol.checkValidString("(*)"))
 print(sol.checkValidString("(*))"))
