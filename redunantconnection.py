2e6852433d65b473b6acec70bb8908609bddea96
Hakase <davidsommerfield.zg@gmail.com>
2023-09-19 20:02:47 -0500

    union find

--- redunantconnection.py
+++ redunantconnection.py
@@ -3,13 +3,31 @@ from typing import List
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        visited = [False] * len(edges)
+        par = list(range(len(edges) + 1))
+        tree_size = [1] * (len(edges) + 1)  # rank
 
-        for e in edges:
-            v1, v2 = e
-            if visited[v1 - 1] and visited[v2 - 1]:
-                return e
-            visited[v1 - 1], visited[v2 - 1] = True, True
+        def find(n):
+            if par[n] != n:
+                par[n] = find(par[n])  # path compression
+            return par[n]
+
+        def union(a, b):
+            p1, p2 = find(a), find(b)
+
+            if p1 == p2:  # can't union because they're already in the same tree
+                return False
+
+            if tree_size[p1] > tree_size[p2]:
+                p1, p2 = p2, p1
+            par[p1] = p2
+            tree_size[p2] += tree_size[p1]
+            return True
+
+        for v1, v2 in edges:
+            if not union(v1, v2):
+                return [v1, v2]
+
+        return []
 
 
 sol = Solution()
