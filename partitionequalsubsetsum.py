053b78f28aa981df990060eab3c89a284c3b1896
Hakase <davidsommerfield.zg@gmail.com>
2023-09-20 04:10:39 -0500

    moer efficient solution

--- partitionequalsubsetsum.py
+++ partitionequalsubsetsum.py
@@ -2,34 +2,30 @@ from typing import List
 
 
 class Solution:
-    def canpartition(self, nums: List[int]) -> bool:
+    def canPartition(self, nums: List[int]) -> bool:
         # what num we're on in the list
         # which list was last added to
-        part1, part2 = [], []
-        stack = [(0, 0)]
+        summation = sum(nums)
+        halfsum = summation // 2
 
-        while stack:
-            i, seen = stack.pop()
+        if summation % 2:
+            return False
 
-            if i == len(nums):
-                if sum(part1) == sum(part2):
+        traversed = [(0, halfsum)]
+
+        while traversed:
+            index, target = traversed.pop()
+
+            if index < len(nums):
+                if target - nums[index] == 0:
                     return True
-            else:
-                if seen == 0:
-                    part1.append(nums[i])
-                elif seen == 1:
-                    part2.append(nums[i])
-                    part1.pop()
-                elif seen == 2:
-                    part2.pop()
 
-                if seen < 2:
-                    stack.append((i, seen + 1))
-
-                stack.append((i + 1, 0))
+                if nums[index] < target:
+                    traversed.append((index + 1, target - nums[index]))
+                traversed.append((index + 1, target))
 
         return False
 
 
 sol = Solution()
-print(sol.canpartition([1, 5, 11, 5]))
+print(sol.canPartition([14, 9, 8, 4, 3, 2]))
