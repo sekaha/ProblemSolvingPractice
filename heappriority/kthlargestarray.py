71829ff82394b11d8dc20d428ae04fb60b5fc869
Hakase <davidsommerfield.zg@gmail.com>
2023-10-29 17:58:57 -0500

    my own heap implementation

--- heappriority/kthlargestarray.py
+++ heappriority/kthlargestarray.py
@@ -1,6 +1,3 @@
-import heapq
-
-
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -8,16 +5,54 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        h = []
+        heap = []
+
+        def heapify_down(i):
+            smallest = i
+
+            left = 2 * i + 1
+            right = 2 * i + 2
+
+            if left < len(heap):
+                if heap[left] < heap[smallest]:
+                    smallest = left
+
+                if right < len(heap):
+                    if heap[right] < heap[smallest]:
+                        smallest = right
+
+                if smallest != i:
+                    heap[smallest], heap[i] = heap[i], heap[smallest]
+                    heapify_down(smallest)
+
+        def heapify_up(i):
+            parent = (i - 1) // 2
+
+            if heap[i] < heap[parent]:
+                heap[i], heap[parent] = heap[parent], heap[i]
+
+                if parent > 0:
+                    heapify_up(parent)
+
+        def heappush(n):
+            heap.append(n)
+            heapify_up(len(heap) - 1)
+
+        def heappop():
+            heap[-1], heap[0] = heap[0], heap[-1]
+            ret = heap.pop()
+            heapify_down(0)
+
+            return ret
 
         for n in nums:
-            heapq.heappush(h, n)
+            heappush(n)
 
-            if len(h) > k:
-                heapq.heappop(h)
+            if len(heap) > k:
+                heappop()
 
-        return heapq.heappop(h)
+        return heappop()
 
 
 sol = Solution()
-print(sol.findKthLargest([3, 2, 1, 5, 6, 4], 2))
+print(sol.findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))
